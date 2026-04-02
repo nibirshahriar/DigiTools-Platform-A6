91c02f4dd5adcf8a91e98f1f5baca05969e8a56e
nibirshahriar <nibir1891@gmail.com>
2026-04-02 14:52:29 +0600

    added humsburger

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,43 @@ import { CiShoppingCart } from "react-icons/ci";
 
 const Navbar = ({ carts }) => {
   return (
-    <div className=" border-b border-gray-100">
+    <div className="border-b border-gray-100">
       <div className="max-w-7xl mx-auto">
         <div className="navbar">
           <div className="navbar-start">
+            <div className="dropdown md:hidden">
+              <div tabIndex={0} role="button" className="btn btn-ghost text-xl">
+                ☰
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-1 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>Products</a>
+                </li>
+                <li>
+                  <a>Features</a>
+                </li>
+                <li>
+                  <a>Pricing</a>
+                </li>
+                <li>
+                  <a>Testimonials</a>
+                </li>
+                <li>
+                  <a>FAQ</a>
+                </li>
+              </ul>
+            </div>
+
             <div className="flex items-center gap-1 font-bold text-xl">
               <span className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">
                 DigiTools
               </span>
             </div>
           </div>
+
           <div className="navbar-center hidden md:flex">
             <ul className="menu menu-horizontal gap-10 px-1 text-lg">
               <li>
@@ -32,6 +59,7 @@ const Navbar = ({ carts }) => {
               </li>
             </ul>
           </div>
+
           <div className="navbar-end gap-5 items-center">
             <div className="indicator">
               <svg
@@ -41,19 +69,20 @@ const Navbar = ({ carts }) => {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                {" "}
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />{" "}
+                />
               </svg>
               <span className="badge badge-sm indicator-item text-red-600">
                 {carts.length}
               </span>
             </div>
+
             <button className="font-bold cursor-pointer">Login</button>
+
             <a className="btn rounded-full text-white bg-linear-to-r from-[#4F39F6] to-[#9514FA] border-none">
               Get Started
             </a>
